c6fa4f0e5665fb800404398e5b3f6222d28bf4b9
evmap <adhitya.ramadhanus@gmail.com>
2016-06-08 19:50:52 +0700

    Refactor code, scrape.js and api.js

--- routes/scrape.js
+++ routes/scrape.js
@@ -118,6 +118,7 @@ router.get('/details', function (req, res) {
       axios.all(axiosGets)
         .then(axios.spread(function () {
           var Responses = Array.prototype.slice.call(arguments)
+          var processed = 0
           Responses.forEach(function (response) {
             var specificJobs = scraper.getJobDetails(response.data)
             var query = {job_id: specificJobs.job_id}
@@ -125,13 +126,14 @@ router.get('/details', function (req, res) {
             var Promise = Jobs.findOneAndUpdate(query,update).exec()
             Promise
               .then(function () {
+                processed++
                 console.log('Done Updating Jobs Detals')
               })
               .catch(function (err) {
                 console.log(err)
               })
           })
-          return res.json({message: 'Scraping Detail Done',updated_jobs: Responses.length})
+          return res.json({message: 'Scraping Detail Done',updated_jobs: processed})
         }))
         .catch(function (response) {
           if (response instanceof Error) {
