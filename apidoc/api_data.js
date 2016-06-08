128c883c8ed45097895da422a54d61d0b2a448bc
evmap <adhitya.ramadhanus@gmail.com>
2016-06-08 21:26:59 +0700

    update documentation and change some code style to comply with standardjs

--- apidoc/api_data.js
+++ apidoc/api_data.js
@@ -120,6 +120,36 @@ define({ "api": [
     "groupTitle": "Jobs"
   },
   {
+    "type": "get",
+    "url": "api/search?q=<search",
+    "title": "keywords> Request all jobs",
+    "name": "GetJobs",
+    "group": "Jobs",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "json",
+            "optional": false,
+            "field": "results",
+            "description": "<p>'All relevant jobs to keywords sorted from highest to lowest'</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n    \"results\":[\n      {\"_id\":\"5752e350cf6a694c1c5cf61d\",\"job_id\":\"161059141\",\"job_name\":\"Process Engineer\",\"company\":\"INNOViON Corporation\",\"location\":\"Wilmington, Massachusetts\",\"score\":0.75},\n      {\"_id\":\"575410d2c88171681f21c053\",\"job_id\":\"Test100\",\"job_name\":\"Software Engineer Test\",\"company\":\"UrbanHire\",\"location\":\"Jakarta\",\"score\":0.6666666666666666},\n      {\"_id\":\"5753d6f6d8c783001f3c761e\",\"job_id\":\"161626738\",\"job_name\":\"System/Software Engineer\",\"company\":\"Sonalysts, Inc.\",\"location\":\"Middletown, Rhode Island\",\"score\":0.6666666666666666},\n      {\"_id\":\"5753d6f6d8c783001f3c7616\",\"job_id\":\"161627182\",\"job_name\":\"Junior System/Software Engineer\",\"company\":\"Sonalysts, Inc.\",\"location\":\"Middletown, Rhode Island\",\"score\":0.625}\n    ]\n  }",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "routes/api.js",
+    "groupTitle": "Jobs"
+  },
+  {
     "type": "post",
     "url": "api/jobs",
     "title": "Create new Job",
@@ -209,7 +239,7 @@ define({ "api": [
       "examples": [
         {
           "title": "Success-Response:",
-          "content": "HTTP/1.1 200 OK\n{\n      \"message\": \"Job Successfully Created!\",\n      \"job\": \n        {\n          \"__v\": 0,\n          \"job_id\": \"Test100\",\n          \"job_name\": \"Software Engineer\",\n          \"company\": \"UrbanHire\",\n          \"logo\": \"mylogo\",\n          \"location\": \"Jakarta\",\n          \"description\": \"NodeJs Engineer\",\n          \"_id\": \"575410d2c88171681f21c053\"\n        }\n    }",
+          "content": "HTTP/1.1 200 OK\n{\n      \"message\": \"Job Successfully Created!\",\n      \"job\":\n        {\n          \"__v\": 0,\n          \"job_id\": \"Test100\",\n          \"job_name\": \"Software Engineer\",\n          \"company\": \"UrbanHire\",\n          \"logo\": \"mylogo\",\n          \"location\": \"Jakarta\",\n          \"description\": \"NodeJs Engineer\",\n          \"_id\": \"575410d2c88171681f21c053\"\n        }\n    }",
           "type": "json"
         }
       ]
@@ -308,7 +338,7 @@ define({ "api": [
       "examples": [
         {
           "title": "Success-Response:",
-          "content": "HTTP/1.1 200 OK\n{\n      \"message\": \"Job Successfully Updated!\",\n      \"job\": \n        {\n          \"__v\": 0,\n          \"job_id\": \"Test100\",\n          \"job_name\": \"Software Engineer Test\",\n          \"company\": \"UrbanHire\",\n          \"logo\": \"mylogo\",\n          \"location\": \"Jakarta\",\n          \"description\": \"NodeJs Engineer\",\n          \"_id\": \"575410d2c88171681f21c053\"\n          \"other_details\" : {}\n        }\n    }",
+          "content": "HTTP/1.1 200 OK\n{\n      \"message\": \"Job Successfully Updated!\",\n      \"job\":\n        {\n          \"__v\": 0,\n          \"job_id\": \"Test100\",\n          \"job_name\": \"Software Engineer Test\",\n          \"company\": \"UrbanHire\",\n          \"logo\": \"mylogo\",\n          \"location\": \"Jakarta\",\n          \"description\": \"NodeJs Engineer\",\n          \"_id\": \"575410d2c88171681f21c053\"\n          \"other_details\" : {}\n        }\n    }",
           "type": "json"
         }
       ]
@@ -316,5 +346,79 @@ define({ "api": [
     "version": "0.0.0",
     "filename": "routes/api.js",
     "groupTitle": "Jobs"
+  },
+  {
+    "type": "get",
+    "url": "scrape/details",
+    "title": "Scrape Job Details from linkedin details page",
+    "name": "GetJobDetails",
+    "group": "Scrape",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "message",
+            "description": "<p>'Scrape Done'</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "inserted_count",
+            "description": "<p>Number of jobs saved</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\"message\": \"Scraping Detail Done\", \"updated_jobs\": 25}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "routes/scrape.js",
+    "groupTitle": "Scrape"
+  },
+  {
+    "type": "get",
+    "url": "scrape/insert",
+    "title": "Scrape Jobs from linkedin listing page",
+    "name": "InsertJobs",
+    "group": "Scrape",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "message",
+            "description": "<p>'Scrape Done'</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "inserted_count",
+            "description": "<p>Number of jobs saved</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\"message\": \"Scrape Done\", \"inserted_count\": 25}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "routes/scrape.js",
+    "groupTitle": "Scrape"
   }
 ] });
--- routes/scrape.js
+++ routes/scrape.js
@@ -1,7 +1,6 @@
 // Standart Test :PASSED
 /* node module dependencies */
 var express = require('express')
-var request = require('request')
 var axios = require('axios')
 var status = require('http-status')
 var scraper = require('../module/scraper')
@@ -13,14 +12,23 @@ var mongoose = require('mongoose')
 var Jobs = mongoose.model('Job')
 // Scraping Endpoint
 // Insert operation, every jobs (if any) will be inserted using bulk insert operation
+/**
+* @api {get} scrape/insert Scrape Jobs from linkedin listing page
+* @apiName InsertJobs
+* @apiGroup Scrape
+* @apiSuccess {String} message 'Scrape Done'
+* @apiSuccess {Number} inserted_count Number of jobs saved
+* @apiSuccessExample Success-Response:
+*     HTTP/1.1 200 OK
+*     {"message": "Scrape Done", "inserted_count": 25}
+*/
 router.get('/insert', function (req, res) {
   var url = req.query.url || 'http://www.linkedin.com/jobs/view-all'
   var config = {
     headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36'}
   }
-  axios.get(url,config)
+  axios.get(url, config)
     .then(function (response) {
-      //console.log(response.data)
       var rawJobs = scraper.getJobs(response.data)
       if (rawJobs.length === 0) return res.json({message: 'Scraping Failed, Html not as expected'})
       var Promise = Jobs.insertMany(rawJobs)
@@ -40,7 +48,7 @@ router.get('/insert', function (req, res) {
         // The request was made, but the server responded with a status code
         // that falls out of the range of 2xx
         console.log('Status Code : ' + response.status)
-        console.log('Response Headers '+JSON.stringify(response.headers))
+        console.log('Response Headers ' + JSON.stringify(response.headers))
         return res.json({error: 'Status code not 200', status: response.status})
       }
     })
@@ -51,25 +59,21 @@ router.get('/update', function (req, res) {
   var config = {
     headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36'}
   }
-  axios.get(url,config)
+  axios.get(url, config)
     .then(function (response) {
-      //console.log(response.data)
       var rawJobs = scraper.getJobs(response.data)
       if (rawJobs.length === 0) return res.json({message: 'Scraping Failed, Html not as expected'})
       var jobQueue = async.queue(function (job, callback) {
         var query = {job_id: job.job_id}
-        var update = {
-          $set:
-            {
-              "job_name": job.job_name,
-              "company": job.company,
-              "logo": job.logo,
-              "location": job.location,
-              "description": job.description
-            }
-          }
+        var update = {$set: {
+          'job_name': job.job_name,
+          'company': job.company,
+          'logo': job.logo,
+          'location': job.location,
+          'description': job.description}
+        }
         var options = {upsert: true}
-        var Promise = Jobs.findOneAndUpdate(query,update,options).exec()
+        var Promise = Jobs.findOneAndUpdate(query, update, options).exec()
         Promise
           .then(function () {
             callback()
@@ -94,26 +98,37 @@ router.get('/update', function (req, res) {
         // The request was made, but the server responded with a status code
         // that falls out of the range of 2xx
         console.log('Status Code : ' + response.status)
-        console.log('Response Headers '+JSON.stringify(response.headers))
+        console.log('Response Headers ' + JSON.stringify(response.headers))
         return res.json({error: 'Status code not 200', status: response.status})
       }
     })
 })
-
+/**
+* @api {get} scrape/details Scrape Job Details from linkedin details page
+* @apiName GetJobDetails
+* @apiGroup Scrape
+* @apiSuccess {String} message 'Scrape Done'
+* @apiSuccess {Number} inserted_count Number of jobs saved
+* @apiSuccessExample Success-Response:
+*     HTTP/1.1 200 OK
+*     {"message": "Scraping Detail Done", "updated_jobs": 25}
+*/
 router.get('/details', function (req, res) {
   // Only take the non scraped jobs details
   var Promise = Jobs.find({other_details: null}, 'job_id').exec()
   Promise
+    // Build an array of axios get request
     .then(function (jobs) {
       var axiosGets = jobs.map(function (job) {
         var url = urlBuilder.buildDetailUrl(job.job_id)
         var config = {
           headers: {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36'}
         }
-        return axios.get(url,config)
+        return axios.get(url, config)
       })
       return axiosGets
     })
+    // Execute the axios get request and scrape every one of them
     .then(function (axiosGets) {
       axios.all(axiosGets)
         .then(axios.spread(function () {
@@ -123,7 +138,7 @@ router.get('/details', function (req, res) {
             if (specificJobs != null) {
               var query = {job_id: specificJobs.job_id}
               var update = {$set: {'other_details': specificJobs.other_details}}
-              var Promise = Jobs.findOneAndUpdate(query,update).exec()
+              var Promise = Jobs.findOneAndUpdate(query, update).exec()
               Promise
                 .then(function () {
                   console.log('Done Updating Jobs Detals')
@@ -133,7 +148,7 @@ router.get('/details', function (req, res) {
                 })
             }
           })
-          return res.json({message: 'Scraping Detail Done',updated_jobs: Responses.length})
+          return res.json({message: 'Scraping Detail Done', updated_jobs: Responses.length})
         }))
         .catch(function (response) {
           if (response instanceof Error) {
@@ -143,7 +158,7 @@ router.get('/details', function (req, res) {
             // The request was made, but the server responded with a status code
             // that falls out of the range of 2xx
             console.log('Status Code : ' + response.status)
-            console.log('Response Headers '+JSON.stringify(response.headers))
+            console.log('Response Headers ' + JSON.stringify(response.headers))
             return res.json({error: 'Status code not 200', status: response.status})
           }
         })
